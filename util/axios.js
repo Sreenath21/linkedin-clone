4624b70b1fd6d06bd4fa2e423c6029b32b508ad1
Sreenath Paramasivam <psreenath0201@gmail.com>
2022-06-17 13:50:13 +0530

    updated

--- util/axios.js
+++ util/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const SITE_URL = "http://localhost:3000";
 
 export const API = axios.create({
-  baseURL: process.env.NEXTAUTH_URL || SITE_URL,
+  baseURL: process.env.NEXTAUTH_URL,
 });
